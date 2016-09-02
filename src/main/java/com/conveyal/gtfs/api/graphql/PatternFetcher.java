a0db5381b91b0c93a12321ff88a3f3e113f2b309
Evan Siroky <evan.siroky@yahoo.com>
2016-09-02 14:41:13 -0700

    Add option to filter GraphQL pattern by pattern_id

--- src/main/java/com/conveyal/gtfs/api/graphql/PatternFetcher.java
+++ src/main/java/com/conveyal/gtfs/api/graphql/PatternFetcher.java
@@ -4,6 +4,7 @@ import com.conveyal.gtfs.api.ApiMain;
 import com.conveyal.gtfs.api.models.FeedSource;
 import com.conveyal.gtfs.model.Pattern;
 import com.conveyal.gtfs.model.Route;
+import com.conveyal.gtfs.model.Stop;
 import com.conveyal.gtfs.model.Trip;
 import graphql.schema.DataFetchingEnvironment;
 
@@ -15,14 +16,54 @@ import java.util.stream.Collectors;
  * Created by matthewc on 3/9/16.
  */
 public class PatternFetcher {
+
+    public static List<WrappedGTFSEntity<Pattern>> apex (DataFetchingEnvironment environment) {
+        Map<String, Object> args = environment.getArguments();
+
+        Collection<FeedSource> feeds;
+
+        List<String> feedId = (List<String>) args.get("feed_id");
+        feeds = feedId.stream().map(ApiMain::getFeedSource).collect(Collectors.toList());
+
+        List<WrappedGTFSEntity<Pattern>> patterns = new ArrayList<>();
+
+        for (FeedSource feed : feeds) {
+            if (args.get("pattern_id") != null) {
+                List<String> patternId = (List<String>) args.get("pattern_id");
+                patternId.stream()
+                        .filter(feed.feed.patterns::containsKey)
+                        .map(feed.feed.patterns::get)
+                        .map(d -> new WrappedGTFSEntity(feed.id, d))
+                        .forEach(patterns::add);
+            } else {
+                feed.feed.patterns.values().stream()
+                        .map(d -> new WrappedGTFSEntity(feed.id, d))
+                        .forEach(patterns::add);
+            }
+        }
+
+        return patterns;
+
+    }
+
     public static List<WrappedGTFSEntity<Pattern>> fromRoute(DataFetchingEnvironment env) {
         WrappedGTFSEntity<Route> route = (WrappedGTFSEntity<Route>) env.getSource();
         FeedSource feed = ApiMain.getFeedSource(route.feedUniqueId);
 
-        return feed.feed.patterns.values().stream()
-                .filter(p -> p.associatedRoutes.contains(route.entity.route_id))
-                .map(p -> new WrappedGTFSEntity<>(feed.id, p))
-                .collect(Collectors.toList());
+        List<String> patternIds = env.getArgument("pattern_id");
+
+        if (patternIds != null) {
+            return feed.feed.patterns.values().stream()
+                    .filter(p -> p.associatedRoutes.contains(route.entity.route_id))
+                    .filter(p -> patternIds.contains(p.pattern_id))
+                    .map(p -> new WrappedGTFSEntity<>(feed.id, p))
+                    .collect(Collectors.toList());
+        } else {
+            return feed.feed.patterns.values().stream()
+                    .filter(p -> p.associatedRoutes.contains(route.entity.route_id))
+                    .map(p -> new WrappedGTFSEntity<>(feed.id, p))
+                    .collect(Collectors.toList());
+        }
     }
 
     public static WrappedGTFSEntity<Pattern> fromTrip(DataFetchingEnvironment env) {
